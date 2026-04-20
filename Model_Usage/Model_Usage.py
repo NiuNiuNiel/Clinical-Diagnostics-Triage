edebd535ccad35a409efbe6abffb163097c63c54
NiuNiuNiel <DanielZFChan@gmail.com>
2026-04-20 19:40:30 +0800

    Model Usage completion

--- Model_Usage/Model_Usage.py
+++ Model_Usage/Model_Usage.py
@@ -6,7 +6,7 @@ import sys
 model_name = sys.argv[0]
 model = load_model(model_name + ".keras")
 
-classes = {"Heartbeat_Abnormility_Model" : ["Normal", 'S', 'V', 'F', 'Q'],
+classes = {"Heartbeat_Abnormality_Model" : ["Normal", 'Supraventricular Ectopic Beats', 'Ventricular Ectopic Beats', 'Fusion Beats', 'Unknown'],
            }
 
 df = exec(sys.argv[1])
@@ -15,8 +15,10 @@ modification = sys.argv[2]
 if modification != None:
     exec(modification)
 
-sample = df.to_numpy()
+if len(df) == 1:
+    prediction = model.predict(df.to_numpy().reshape(1, -1, 1))
+    print({"Prediction":classes[model_name][np.argmax(prediction, axis=-1)[0]], "Confident Score":f"{np.max(prediction, axis=-1)[0]*100:.2f}%"})
 
-prediction = model.predict(sample.reshape(1, -1, 1))
-
-print({"Prediction":classes[model_name][np.argmax(prediction, axis=-1)[0]], "Confident Score":f"{np.max(prediction, axis=-1)[0]*100:.2f}%"})
+else:
+    predictions = model.predict(df.values)
+    print([{"Index":index, "Prediction":classes[model_name][np.argmax(prediction, axis=-1)[0]], "Confident Score":f"{np.max(prediction, axis=-1)[0]*100:.2f}%"} for index,prediction in enumerate(predictions)])
