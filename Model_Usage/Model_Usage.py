36589eed77d1f6e171c8ed07d2b0afce86b70cdb
NiuNiuNiel <DanielZFChan@gmail.com>
2026-04-21 11:15:29 +0800

    Image Features

--- Model_Usage/Model_Usage.py
+++ Model_Usage/Model_Usage.py
@@ -5,6 +5,9 @@ import numpy as np
 import ast
 import sys
 import zai
+from PIL import Image
+
+
 
 user_prompt = sys.argv[1]
 files_attached = sys.argv[2:]
@@ -41,7 +44,9 @@ Each field is defined as follows:
 - "requires_model": A boolean indicating whether a model is required for the task.
 - "AI_nodes": A list of objects, where each object represents an individual processing task. This allows the system to process multiple files or apply different models in a single request. Each object in the list must contain the specific configuration for that file.
 - "model_name": The name of the model to be used for this specific node.
-- "modification": Any modifications to be applied on the data with the variable name "df", E.g., df.replace(parameters), df.map(parameters), df.iloc[parameters] = "<value>", etc. null if no modifications are needed.
+- "modification": Any modifications to be applied on the data.
+    - For Tabular/Signal data (csv,tsv,json,...): Choose ONLY from "forward_fill", "fill_zero", "drop_missing", or None if no cleaning is needed.
+    - For Image data (png,jpg,jpeg): Set to None (the backend automatically handles standard tensor reshaping)
 - "file_path": The path of the file being processed in this node.
 - "headers": An integer indicating the row number to be used as headers for this file, or null if there are no headers.
 """
@@ -52,7 +57,7 @@ model_choosing_response = GLM_model.chat.completions.create(
     stream=True
 )
 
-model_choosing = ast.literal_eval(model_choosing_response)
+model_choosing = ast.literal_eval(model_choosing_response.choises[0].message.content)
 
 def read_file(file_path, headers):
     file_type = file_path.split(".")[-1].lower()
@@ -65,25 +70,40 @@ def read_file(file_path, headers):
         return pd.read_csv(file_path, sep="\t", header=headers)
     elif file_type == "json":
         return pd.read_json(file_path)
+    elif file_type in ["png", "jpg", "jpeg"]:
+        image = Image.open(file_path)
+        return np.asarray(image)
     else:
         raise ValueError("Unsupported file type")
 
+def modification_func(data, modification):
+    if modification == "forward_fill":
+        return data.ffill()
+    elif modification == "fill_zero":
+        return data.fillna(0)
+    elif modification == "drop_missing":
+        return data.dropna()
+    else:
+        raise ValueError("Unsupported modification type")
 
 def AI_node(model_name, modification, file_path, headers):
     model = load_model(model_name + ".keras")
 
-    df = read_file(file_path, headers=headers)
+    file_data = read_file(file_path, headers=headers)
 
     if modification != None:
-        exec(modification)
+        file_data = modification_func(file_data, modification)
 
     file_name = file_path.split("/")[-1]
 
-    if len(df) == 1:
-        prediction = model.predict(df.to_numpy().reshape(1, -1, 1))[0]
+    if file_path.split(".")[-1].lower() in ["png", "jpg", "jpeg"]:
+        return
+
+    if len(file_data) == 1:
+        prediction = model.predict(file_data.to_numpy().reshape(1, -1, 1))[0]
         return {file_name:{"Prediction":classes[model_name][np.argmax(prediction, axis=-1)], "Confident Score":f"{np.max(prediction, axis=-1)*100:.2f}%"}}
 
-    predictions = model.predict(df.values)
+    predictions = model.predict(file_data.values)
     return {file_name:[{"Index":index, "Prediction":classes[model_name][np.argmax(prediction, axis=-1)], "Confident Score":f"{np.max(prediction, axis=-1)*100:.2f}%"} for index,prediction in enumerate(predictions)]}
 
 if model_choosing["requires_model"]:
